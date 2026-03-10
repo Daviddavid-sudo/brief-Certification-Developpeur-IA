64bb3ad2edba6713800c1a5f1d432baf79b00f08
david <david.scott.2875@gmail.com>
2026-03-10 11:48:14 +0100

    added tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class BasicTests(TestCase):
+    def test_home_page_status_code(self):
+        # Checks if the URL named 'population_view' (or whatever your home is) works
+        # If this returns a 500 error because of a typo, the CI will FAIL.
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_db_is_accessible(self):
+        # Verifies the CI can actually talk to your SQLite DB
+        from dashboard.models import Population  # Replace with one of your actual models
+        count = Population.objects.count()
+        self.assertGreaterEqual(count, 0)
