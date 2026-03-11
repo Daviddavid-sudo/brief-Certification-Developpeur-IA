9f8e925e4f085073b646e11c35b3612b10f2d2c6
david <david.scott.2875@gmail.com>
2026-03-11 13:42:23 +0100

    fixed

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -95,7 +95,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
+        'NAME': 'certificate_dev',
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'postgres'),  # default to 'postgres' service
