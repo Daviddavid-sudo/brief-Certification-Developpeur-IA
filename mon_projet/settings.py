bdd30be0ce88864567ecb956cb3aace753b4d246
david <david.scott.2875@gmail.com>
2026-08-27 11:39:37 +0200

    updated databases

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -89,9 +89,7 @@ WSGI_APPLICATION = "mon_projet.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
+        default=os.getenv("DATABASE_URL")
     )
 }
 
